af8aba6356d8193ebee7f64fe6c2ef46da360007
Marvin <a7351220@gmail.com>
2025-02-13 19:16:55 +0800

    build: configure production build process
    - Add TypeScript build configuration
    - Move TypeScript from devDependencies to dependencies
    - Add build and clean scripts
    - Update start script to use compiled JavaScript
    - Add @types/node dependency

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44e3efbd906ddc63"; }
+/******/ 		__webpack_require__.h = function() { return "c123e9a81e7e9d55"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
