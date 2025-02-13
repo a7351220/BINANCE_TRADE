5815c9ef754358029d8ed6123646707c05218855
Marvin <a7351220@gmail.com>
2025-02-13 21:42:39 +0800

    feat: add website title and favicon
    - Add page title 'LiqData - Real-time Market Data'
    - Add website description meta tag
    - Configure custom logo.png as favicon
    - Improve SEO and branding

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c123e9a81e7e9d55"; }
+/******/ 		__webpack_require__.h = function() { return "1ba97921a2e2148a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>LiqData - Real-time Market Data</title>
+        <link rel="icon" href="/logo.png" />
+        <meta name="description" content="Real-time cryptocurrency market data and analysis" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp 
